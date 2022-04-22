7aa8ba5a64dd4b9b643c3d93c1af0dbe372dbe16
Ellen Rocha <ellen_rochafarias@hotmail.com>
2022-04-22 18:50:33 -0300

    adicionamento do carrosel e redirecionamento de paginas nos botões

---
+++ Js/setup.js
@@ -0,0 +1,32 @@
+$('.owl-carousel').owlCarousel({
+  loop:true,
+  margin:10,
+  nav:false,
+  responsive:{
+      0:{
+          items:1
+      },
+      600:{
+          items:3
+      },
+      1000:{
+          items:5
+      }
+  }
+})
+
+function info() {
+window.open("https://www.themoviedb.org/tv/124834-heartstopper?language=pt-BR")
+}
+function assista(){
+  window.open("https://www.youtube.com/watch?v=hTyXVn9t0ig&t=22s")
+}
+function series(){
+  window.open("https://www.adorocinema.com/streaming/netflix/series/melhores/")
+}
+function filmes(){
+  window.open("https://www.adorocinema.com/streaming/netflix/filmes/melhores/")
+}
+function documentarios(){
+  window.open("https://www.netflix.com/br/browse/genre/6839")
+}
